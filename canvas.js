0254f3205a25a5ab249eac15cb990cebff3bd139
Yuqiwu <ywu@stuy.edu>
2018-02-07 18:32:48 -0500

    fixed merge conflict

--- canvas.js
+++ canvas.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var box = document.getElementById("box");
 var ctx = box.getContext("2d");
 var stop = document.getElementById("stop");
@@ -13,7 +12,7 @@ var clear = function(){
 
 var stop = function(){
     window.cancelAnimationFrame( requestID );
-};
+}
 
 var draw = function(){
     r = 0;
@@ -38,53 +37,8 @@ var draw = function(){
 	ctx.fill();
 	x--;
     }
-};
-
-
+}
 
 stop.addEventListener("click", stop);
-=======
-var box = document.getElementById("box")
-var ctx = box.getContext("2d")
-var c = document.getElementById("clear")
-var t = document.getElementById("toggle")
-var cond = 0;
-
-var clear = function(e){
-    ctx.clearRect(0, 0, 500, 500);
-    console.log("clear");
-}
-
-var toggle = function(e){
-    if (cond == 0){
-	cond = 1;
-    }
-    else{
-	cond = 0;
-    }
-    console.log(cond);
-}
-
-var draw = function(e){
-    ctx.fillStyle = "red";
-    if (cond == 0){
-	ctx.beginPath();
-	ctx.arc(e.clientX - 8, e.clientY - 51, 15, 0, 2 * Math.PI);
-	ctx.stroke();
-	ctx.fill();
-	console.log("draw a circle");
-    }
-    else{
-	ctx.fillRect(e.clientX - 23, e.clientY - 66, 30, 30);
-	console.log("draw a rectangle");
-    }
-    console.log("x = " + e.clientX);
-    console.log("y = " + e.clientY);
-}
-
-
-
-c.addEventListener("click", clear);
-t.addEventListener("click", toggle);
->>>>>>> 545f081cd3ad6e063aa9926e1edffe3797cf7a5e
 box.addEventListener("click", draw);
+
